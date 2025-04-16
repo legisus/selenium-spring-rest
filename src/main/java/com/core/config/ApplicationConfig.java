8650d0a89f413733d165626824634ee2fe2b0a00
legisus <legisus@gmail.com>
2025-04-16 14:39:19 -0800

    cors

--- src/main/java/com/core/config/ApplicationConfig.java
+++ src/main/java/com/core/config/ApplicationConfig.java
@@ -1,13 +1,12 @@
 package com.core.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class ApplicationConfig {
-    /**
-     * Configure CORS for the API endpoints
-     */
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
